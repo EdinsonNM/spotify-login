081260d29116de82a86165c68d0254f8b6467ba6
Edinson Nuñez More <nmedinson@gmail.com>
2021-03-09 00:19:17 -0500

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = (webpackConfigEnv, argv) => {
       },
     },
   })(defaultConfig, {
+    externals: [/^@ps\/.+/],
     // customize the webpack config here
     plugins: [new dotEnv()],
     module: {
